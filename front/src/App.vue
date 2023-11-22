5e4e0ba6b9d5cefba00ac3c3dcc1d62b8ca00671
Berol Bertin <124741895+berol24@users.noreply.github.com>
2023-11-22 20:40:28 +0100

    correction de fichier

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,57 +1,30 @@
-<!-- <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
-
-
 <script>
 
 </script>
 
 <template>
-  <div class="accueil">
+ <div class="accueil">   
     <section-left class="left" />
-    <section-center class="center" />
-    <section-right class="right" />
-  </div>
+    <section-center class="center"/>
+    <section-right class="right"/>
+ </div>
+
 </template>
 
 <style scoped>
-.accueil {
+
+.accueil{
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.left,
-.center,
-.right {
+.left , .center , .right {
   margin: 8px;
 }
 
-.left {
-  width: 469px;
+.left{
+  width: 469px; 
 }
+
 </style>
