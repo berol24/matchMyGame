eb931408faa88a0e11f13204f818aef2bdf69ff0
Makhtar99 <127096437+Makhtar99@users.noreply.github.com>
2023-11-27 10:49:50 +0100

    Maj

--- MatchMyGame/app/Http/Controllers/ValorantController.php
+++ MatchMyGame/app/Http/Controllers/ValorantController.php
@@ -2,7 +2,7 @@
 
 use App\Services\ValorantService;
 
-public function getValorantPlayerData(ValorantService $valorantService, $username)
+function getValorantPlayerData(ValorantService $valorantService, $username)
 {
     $playerData = $valorantService->getPlayerData($username);
     return view('valorant-player-profile', ['playerData' => $playerData]);
